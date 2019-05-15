5db118919b863ad0255105a680507e584de6d142
Edgar F <eflores.ef@gmail.com>
2019-05-14 19:24:28 -0700

    initial commit, set up

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
+
+server.use(logger);
+server.use(validateUserId);
+server.use(validateUser);
+server.use(validatePost);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -11,5 +16,17 @@ server.get('/', (req, res) => {
 function logger(req, res, next) {
 
 };
+
+function validateUserId(req, res, next) {
+
+}
+
+function validateUser(req, res, next) {
+
+}
+
+function validatePost(req, res, next) {
+
+}
 
 module.exports = server;
